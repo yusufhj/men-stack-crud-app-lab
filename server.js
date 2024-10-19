f608f0e48ac78ae3e3b23c4eb58bfa6ee52ba8d3
Yusuf AlHamad <ylive71@gmail.com>
2024-10-19 21:50:13 +0300

    initialization: configured and initial route and connection works

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const path = require("path")
 
-const plantSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  image: String,
-})
 
-const Plant = mongoose.model('Plant', plantSchema)
-module.exports = Plant
+const app = express();
+const Plant = require("./models/plant");
+const plantsCtrl = require('./controllers/plants')
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, "public")))
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello, plants!");
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
